8e6a74093bd7717a0e67e925c140eeb55372d94e
Ansar331 <95215784+Ansar331@users.noreply.github.com>
2021-12-27 20:14:41 +0600

    Update mario.py

--- mario.py
+++ mario.py
@@ -1,1 +1,53 @@
+import pygame
 
+pygame.init()
+width = 1000
+height = 600
+speed = 60
+sc = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Mario")
+clock = pygame.time.Clock()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill('GREEN')
+        self.rect = self.image.get_rect()
+        self.rect.centerx = width / 2
+        self.rect.bottom = height - 10
+        self.speed_x = 0
+
+    def update(self):
+        self.speed_x = 0
+        key = pygame.key.get_pressed()
+        if key[pygame.K_LEFT]:
+            self.speed_x = -5
+        if key[pygame.K_RIGHT]:
+            self.speed_x = 5
+        self.rect.x += self.speed_x
+        if self.rect.right > width:
+            self.rect.right = width
+        if self.rect.left < 0:
+            self.rect.left = 0
+
+
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
+
+run = True
+while run:
+    clock.tick(speed)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    all_sprites.update()
+
+    sc.fill('BLACK')
+    all_sprites.draw(sc)
+    pygame.display.flip()
+
+pygame.quit()
