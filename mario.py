80e4614d6c39a14846ac7df691ab86afcd984ce4
Ansar331 <95215784+Ansar331@users.noreply.github.com>
2021-12-30 14:31:41 +0600

    Update mario.py

--- mario.py
+++ mario.py
@@ -4,9 +4,13 @@ pygame.init()
 width = 1000
 height = 600
 speed = 60
+is_jump = True
+vec = pygame.math.Vector2
 sc = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Mario")
+pygame.display.set_caption("Aario")
 clock = pygame.time.Clock()
+pygame.mixer.music.load('music.mp3')
+pygame.mixer.music.play(-1)
 
 
 class Player(pygame.sprite.Sprite):
@@ -17,6 +21,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.centerx = width / 2
         self.rect.bottom = height - 10
+        self.vel = vec(0,0)
         self.speed_x = 0
 
     def update(self):
@@ -28,13 +33,14 @@ class Player(pygame.sprite.Sprite):
             self.rect.bottom += 5
         if key[pygame.K_LEFT]:
             self.speed_x = -5
+
         if key[pygame.K_RIGHT]:
             self.speed_x = 5
         self.rect.x += self.speed_x
         if self.rect.right > width:
             self.rect.right = width
-        if self.rect.bottom > 600:
-            self.rect.bottom = 600
+        if self.rect.bottom > height:
+            self.rect.bottom = height
         if self.rect.top < 0:
             self.rect.top = 0
         if self.rect.left < 0:
@@ -53,7 +59,6 @@ while run:
             run = False
 
     all_sprites.update()
-
     sc.fill('BLACK')
     all_sprites.draw(sc)
     pygame.display.flip()
