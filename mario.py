b4c4a67649be32796f8fa416533a35019057a83d
Ansar331 <95215784+Ansar331@users.noreply.github.com>
2022-01-01 19:23:59 +0600

    Update mario.py

--- mario.py
+++ mario.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 player_img = pygame.image.load('mario.png')
 player_img.set_colorkey('White')
@@ -15,78 +16,15 @@ speed = 60
 sc = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Aario")
 clock = pygame.time.Clock()
-pygame.mixer.music.load('music.mp3')
-pygame.mixer.music.play(-1)
+
 sc.fill((255, 255, 255))
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y, obstacles):
         pygame.sprite.Sprite.__init__(self)
+        self.obstacles = obstacles
         self.image = player_img2
-        self.rect = self.image.get_rect()
-        self.rect.centerx = width / 2
-        self.rect.bottom = height - 10
-        self.speed_x = 0
-
-    def update(self):
-        self.speed_x = 0
-        key = pygame.key.get_pressed()
-        if key[pygame.K_UP]:
-            self.rect.bottom -= 5
-        if key[pygame.K_DOWN]:
-            self.rect.bottom += 5
-        if key[pygame.K_LEFT]:
-            self.speed_x = -5
-        if key[pygame.K_RIGHT]:
-            self.speed_x = 5
-        self.rect.x += self.speed_x
-        if self.rect.right > width:
-            self.rect.right = width
-        if self.rect.bottom > height:
-            self.rect.bottom = height
-        if self.rect.top < 0:
-            self.rect.top = 0
-        if self.rect.left < 0:
-            self.rect.left = 0
-
-
-class Mushroom(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = mushroom_img2
-        self.rect = self.image.get_rect()
-        self.rect.x = 1400
-        self.rect.y = 890
-        self.speed_x = 0
-
-    def update(self):
-        self.speed_x = 0
-        key = pygame.key.get_pressed()
-        if key[pygame.K_UP]:
-            self.rect.bottom -= 5
-        if key[pygame.K_DOWN]:
-            self.rect.bottom += 5
-        if key[pygame.K_LEFT]:
-            self.speed_x = -5
-        if key[pygame.K_RIGHT]:
-            self.speed_x = 5
-        self.rect.x += self.speed_x
-        if self.rect.right > width:
-            self.rect.right = width
-        if self.rect.bottom > height:
-            self.rect.bottom = height
-        if self.rect.top < 0:
-            self.rect.top = 0
-        if self.rect.left < 0:
-            self.rect.left = 0
-
-
-class Turtle(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((50, 50))
-        self.image.fill('RED')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -102,6 +40,10 @@ class Turtle(pygame.sprite.Sprite):
         if key[pygame.K_LEFT]:
             self.speed_x = -5
         if key[pygame.K_RIGHT]:
+            print('right', self.rect.right, 'bottom', self.rect.top)
+            if self.rect.x == 1100 and self.rect.bottom > 750:
+                print('test')
+                return
             self.speed_x = 5
         self.rect.x += self.speed_x
         if self.rect.right > width:
@@ -114,27 +56,52 @@ class Turtle(pygame.sprite.Sprite):
             self.rect.left = 0
 
 
+class Mushroom(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = mushroom_img2
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed_x = 0
+
+    def update(self):
+        pass
+
+
+class Turtle(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50, 50))
+        self.image.fill('RED')
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed_x = 0
+
+    def update(self):
+        self.speed_x = 0
+
+
 class Pills(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 50))
         self.image.fill('BLUE')
         self.rect = self.image.get_rect()
-        self.rect.centerx = 900
-        self.rect.bottom = 890
+        self.rect.x = x
+        self.rect.y = y
         self.speed_x = 0
 
-
-mushroom = Mushroom()
-pill = Pills()
+mushroom = Mushroom(1500, 800)
+pill = Pills(0, 0)
 turtle = Turtle(300, 890)
 all_sprites = pygame.sprite.Group()
-player = Player()
+player = Player(0, 800, obstacles=[(1200, 150),])
 all_sprites.add(player)
 all_sprites.add(mushroom)
 all_sprites.add(turtle)
 all_sprites.add(pill)
-
 run = True
 while run:
     clock.tick(speed)
@@ -144,7 +111,9 @@ while run:
 
     all_sprites.update()
     sc.fill('BLACK')
+    pygame.draw.rect(sc, 'WHITE', (1200, 750, 50, 150))
     all_sprites.draw(sc)
     pygame.display.flip()
-
+    pygame.init()
 pygame.quit()
+    
