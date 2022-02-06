3f5239b2f6614b061933547f6bdab885a514df34
Ansar331 <95215784+Ansar331@users.noreply.github.com>
2022-02-06 22:01:19 +0600

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,9 +1,12 @@
+from ast import PyCF_ALLOW_TOP_LEVEL_AWAIT
 import json
 import time
 import pygame
 import random
+import threading
 
 pygame.init()
+boss_group = pygame.sprite.Group()
 
 
 class GameSprite(pygame.sprite.Sprite):
@@ -22,7 +25,21 @@ class Mushrom(GameSprite):
         self.dir = None
 
 
+class Boss(GameSprite):
+    def __init__(self, x, y, image):
+        GameSprite.__init__(self, x, y, image)
+        self.move_cnt = 0
+        self.dir = None
+
+
 class Bomb(GameSprite):
+    def __init__(self, x, y, image):
+        GameSprite.__init__(self, x, y, image)
+        self.move_cnt = 0
+        self.dir = None
+
+
+class Turtle(GameSprite):
     def __init__(self, x, y, image):
         GameSprite.__init__(self, x, y, image)
         self.move_cnt = 0
@@ -53,7 +70,7 @@ class Key(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 50))
-        self.image.fill('RED')
+        self.image.fill('GREen')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -63,7 +80,7 @@ class Key2(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((50, 50))
-        self.image.fill('RED')
+        self.image.fill('GREen')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -90,9 +107,8 @@ player_image = GameImage(110, 110, 'White', 'mario.png')
 mushroom_image = GameImage(90, 90, 'White', 'mushroom.png')
 turtle_image = GameImage(150, 90, 'White', 'turtle.png')
 pill_image = GameImage(150, 100, 'White', 'pill.png')
-boss_image = GameImage(1000, 500, 'WHITE', 'boss.jpg')
-bomb_image = GameImage(90, 90, 'WHITE', 'bomb.jpg')
-boss_group = pygame.sprite.Group()
+bomb_image = GameImage(90, 90, 'WHITE', 'bomb.png')
+boss_image = GameImage(1000, 300, 'WHITE', 'boss.jpg')
 
 
 class GameSettings:
@@ -125,6 +141,7 @@ class GameSettings:
             sprite.add(turtle)
         for boss in self.bosses:
             sprite.add(boss)
+            boss_group.add(boss)
 
     def remove_from_sprite(self, sprite):
         for obs in self.obstacles:
@@ -152,14 +169,21 @@ class GameSettings:
         bombs = []
         bombs_info = self.settings.get('bombs')
         for gi in bombs_info:
-            bombs.append(GameSprite(gi[0], gi[1], bomb_image.get_image()))
+            bombs.append(Bomb(gi[0], gi[1], bomb_image.get_image()))
         self.bombs = bombs
+
+    def init_bosses(self):
+        bosses = []
+        bosses_info = self.settings.get('bosses')
+        for gi in bosses_info:
+            bosses.append(Boss(gi[0], gi[1], boss_image.get_image()))
+        self.bosses = bosses
 
     def init_turtles(self):
         turtles = []
         turtles_info = self.settings.get('turtles')
-        for ui in turtles_info:
-            turtles.append(GameSprite(ui[0], ui[1], turtle_image.get_image()))
+        for gi in turtles_info:
+            turtles.append(Turtle(gi[0], gi[1], turtle_image.get_image()))
         self.turtles = turtles
 
     def init_balls(self):
@@ -183,28 +207,22 @@ class GameSettings:
             mushrooms.append(Mushrom(di[0], di[1], mushroom_image.get_image()))
         self.mushrooms = mushrooms
 
-    def init_bosses(self):
-        bosses = []
-        bosses_info = self.settings.get('bosses')
-        for di in bosses_info:
-            bosses.append(Mushrom(di[0], di[1], boss_image.get_image()))
-        self.bosses = bosses
-
 
 mushroom_group = pygame.sprite.Group()
 turtle_group = pygame.sprite.Group()
+
 bullets = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 
 run = True
-game_settings = GameSettings(height=900, width=1800, speed=60, level=1)
+game_settings = GameSettings(height=900, width=1800, speed=120, level=1)
 game_settings.init_game()
 
 sc = pygame.display.set_mode((game_settings.width, game_settings.height))
 pygame.display.set_caption("Aario")
 
 clock = pygame.time.Clock()
-used = False
+
 right_side = False
 left_side = False
 
@@ -231,7 +249,6 @@ class Player(pygame.sprite.Sprite):
         self.isJump = False
         self.jumpCount = 10
         self.jump_height = 500
-
 
     def update(self):
         key = pygame.key.get_pressed()
@@ -277,6 +294,18 @@ class Player(pygame.sprite.Sprite):
                 self.game_settings.mushrooms.remove(mushroom)
                 all_sprites.remove(mushroom)
 
+    def remove_killed_turtle(self):
+        for turtle in self.game_settings.turtles:
+            if self.rect.bottom == turtle.rect.top and abs(self.rect.centerx - turtle.rect.centerx) < self.rect.width:
+                self.game_settings.turtles.remove(turtle)
+                all_sprites.remove(turtle)
+
+    def remove_killed_bomb(self):
+        for bomb in self.game_settings.bombs:
+            if self.rect.bottom == bomb.rect.top and abs(self.rect.centerx - bomb.rect.centerx) < self.rect.width:
+                self.game_settings.bombs.remove(bomb)
+                all_sprites.remove(bomb)
+
     def jump(self):
         if self.isJump:
             if self.jumpCount >= -10:
@@ -304,6 +333,8 @@ class Player(pygame.sprite.Sprite):
     def jump_task(self):
         for i in range(self.jump_height):
             self.remove_killed_mushrooms()
+            self.remove_killed_turtle()
+            self.remove_killed_bomb()
             new_y = self.rect.y - 1 if i <= self.jump_height // 2 else self.rect.y + 1
             new_rect = pygame.Rect(self.rect.x, new_y, self.rect.width, self.rect.height)
             for obs in self.game_settings.obstacles:
@@ -364,19 +395,19 @@ class BulletRight(pygame.sprite.Sprite):
 
 
 pill = GameSprite(1000, 800, pill_image.get_image())
-key1 = Key(500, 800)
-key2 = Key2(1200, 800)
-
+key1 = Key(1700, 800)
+key2 = Key2(400, 400)
 player = Player(0, 800, player_image.get_image(), game_settings=game_settings, width=1800, height=900)
 
 f6 = pygame.font.Font(None, 75)
-text6 = f6.render('ОЧКИ:', True, 'White')
+text16 = f6.render('ОЧКИ:', True, 'White')
 f7 = pygame.font.Font(None, 75)
 text7 = f7.render('НУЖНО ОЧКОВ:', True, 'White')
 f8 = pygame.font.Font(None, 75)
 text8 = f8.render('МЯЧЕЙ:', True, 'White')
 
 weapon = pygame.sprite.Group()
+
 
 all_sprites.add(player, mushroom_group, turtle_group, pill)
 all_sprites.add(key1, key2)
@@ -403,6 +434,40 @@ def move_mushroom(mushroom, obstacle, n, m):
     return mushroom
 
 
+def move_turtle(turtle, obstacle, n, m):
+    if turtle.move_cnt == 0 or not turtle.dir:
+        turtle.dir = random.choice([-1, 1])
+        turtle.move_cnt = random.randint(30, 50)
+    ny = turtle.rect.left + turtle.dir if turtle.dir == -1 else turtle.rect.right + turtle.dir
+    if turtle.dir == -1 and ny < 0 or turtle.dir == 1 and ny > 1800:
+        turtle.move_cnt = 0
+        move_turtle(turtle, obstacle, 1800, 900)
+    for obs in obstacle:
+        if turtle.dir == -1 and ny == obs.rect.right or turtle.dir == 1 and ny == obs.rect.left:
+            turtle.move_cnt = 0
+            move_turtle(turtle, obstacle, 1800, 900)
+    turtle.move_cnt -= 1
+    turtle.rect.left += turtle.dir
+    return turtle
+
+
+def move_bomb(bomb, obstacle, n, m):
+    if bomb.move_cnt == 0 or not bomb.dir:
+        bomb.dir = random.choice([-1, 1])
+        bomb.move_cnt = random.randint(30, 50)
+    ny = bomb.rect.left + bomb.dir if bomb.dir == -1 else bomb.rect.right + bomb.dir
+    if bomb.dir == -1 and ny < 0 or bomb.dir == 1 and ny > 1800:
+        bomb.move_cnt = 0
+        move_bomb(bomb, obstacle, 1800, 900)
+    for obs in obstacle:
+        if bomb.dir == -1 and ny == obs.rect.right or bomb.dir == 1 and ny == obs.rect.left:
+            bomb.move_cnt = 0
+            move_bomb(bomb, obstacle, 1800, 900)
+    bomb.move_cnt -= 1
+    bomb.rect.left += bomb.dir
+    return bomb
+
+
 def increase_level():
     game_settings.remove_from_sprite(all_sprites)
     game_settings.increase_level()
@@ -421,6 +486,7 @@ def main():
     a1 = 1
     a2 = 1
     a3 = 1
+    used = False
 
     f15 = pygame.font.Font(None, 150)
     run = True
@@ -432,7 +498,8 @@ def main():
                 run = False
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
-                    player.isJump = True
+                    jump_task = threading.Thread(target=player.jump_task, args=())
+                    jump_task.start()
                 if event.key == pygame.K_e:
                     balls += 1
                     player.shoot()
@@ -442,18 +509,16 @@ def main():
 
         for ball in game_settings.balls:
             if pygame.sprite.collide_rect(player, ball):
-                print(1)
                 ochko += 20
                 all_sprites.remove(ball)
                 game_settings.balls.remove(ball)
-
         sc.fill('black')
         f9 = pygame.font.Font(None, 75)
         text9 = f9.render(str(balls), True, 'White')
         f10 = pygame.font.Font(None, 75)
         text10 = f10.render(str(ochko), True, 'White')
 
-        sc.blit(text6, (00, 0))
+        sc.blit(text16, (00, 0))
         sc.blit(text8, (1300, 0))
         sc.blit(text9, (1550, 0))
         sc.blit(text10, (200, 0))
@@ -469,20 +534,27 @@ def main():
             neg = 4
             used = True
 
+        player.remove_killed_mushrooms()
+        player.remove_killed_turtle()
         for mushroom in game_settings.mushrooms:
-            if player.rect.bottom == mushroom.rect.top and \
-                abs(player.rect.centerx - mushroom.rect.centerx) < 110:
-                mushroom = None
-                break
-            new_rect = pygame.Rect(player.rect.x, player.rect.y, player.rect.width, player.rect.height)
-            if mushroom.rect.y >= player.rect.y:
-                new_rect.y -= 5
-            else:
-                new_rect.y += 5
-            if new_rect.colliderect(mushroom.rect):
-                over = True
-                break
+            if player.rect.colliderect(mushroom.rect):
+                if not used:
+                    over = True
+                    break
             move_mushroom(mushroom, game_settings.obstacles, game_settings.height, game_settings.width)
+        for turtle in game_settings.turtles:
+            if player.rect.colliderect(turtle.rect):
+                if not used:
+                    over = True
+                    break
+            move_turtle(turtle, game_settings.obstacles, game_settings.height, game_settings.width)
+        for bomb in game_settings.bombs:
+            if player.rect.colliderect(bomb.rect):
+                if not used:
+                    over = True
+
+                    break
+            move_bomb(bomb, game_settings.obstacles, game_settings.height, game_settings.width)
 
         if pygame.sprite.groupcollide(boss_group, bullets, False, True):
             hp -= 1
@@ -501,16 +573,11 @@ def main():
                 a3 = 0
             else:
                 pass
-        if pygame.sprite.groupcollide(bullets, boss_group, True, False):
-            hp -= 1
-        if pygame.sprite.groupcollide(turtle_group, bullets, False, True):
-            pass
 
         if hp <= 0:
             pygame.init()
             return ochko, hp, balls
         if over:
-            scr = pygame.display.set_mode((1800, 900))
 
             f1 = pygame.font.Font(None, 100)
             text1 = f1.render('ВЫ ПРОИГРАЛИ', True, 'White')
@@ -522,20 +589,20 @@ def main():
             text4 = f4.render('Escape', True, 'White')
             f5 = pygame.font.Font(None, 70)
             text5 = f5.render('КОЛИЧЕСТВО ОЧКОВ:', True, 'WHITE')
-            f20 =pygame.font.Font(None, 90)
-            text20 = f20.render(str(ochko), True, 'White')
-            f21 = pygame.font.Font(None, 90)
-            text21 = f21.render(str(balls), True, 'WHITE')
+            f6 = pygame.font.Font(None, 90)
+            text6 = f6.render(str(ochko), True, 'White')
+            f7 = pygame.font.Font(None, 90)
+            text7 = f7.render(str(balls), True, 'WHITE')
 
-            no = pygame.draw.rect(scr, 'RED', (750, 550, 300, 100))
+            no = pygame.draw.rect(sc, 'RED', (750, 550, 300, 100))
 
-            scr.blit(text1, (630, 50))
-            scr.blit(text2, (50, 200))
-            scr.blit(text3, (820, 560))
-            scr.blit(text4, (830, 600))
-            scr.blit(text5, (50, 300))
-            scr.blit(text20, (700, 300))
-            scr.blit(text21, (700, 200))
+            sc.blit(text1, (630, 50))
+            sc.blit(text2, (50, 200))
+            sc.blit(text3, (820, 560))
+            sc.blit(text4, (830, 600))
+            sc.blit(text5, (50, 300))
+            sc.blit(text6, (700, 300))
+            sc.blit(text7, (700, 200))
             pygame.display.update()
             key = pygame.mouse.get_pressed()
 
@@ -547,5 +614,6 @@ def main():
                         run = False
                     if key[pygame.K_ESCAPE]:
                         run = False
-        player.jump()
+            pygame.quit()
+
     pygame.quit()
